c8253e9220e04677f1afead2e2431b1ea826af74
Parham Mootab <psm1379@gmail.com>
2022-11-08 13:01:34 -0800

    initial commit

---
+++ Shipfastr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shipfastr
+//
+//  Created by Parham Shagerdmootab on 2022-11-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shipfastr/ShipfastrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShipfastrApp.swift
+//  Shipfastr
+//
+//  Created by Parham Shagerdmootab on 2022-11-08.
+//
+
+import SwiftUI
+
+@main
+struct ShipfastrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
