fc380f5087acc4a8d088c51738b4e2b9536b5bb2
m2rads <argo.mohrad@gmail.com>
2022-11-12 17:44:41 -0800

    add dark theme map stlying

--- Shipfastr/Views/MapView/MapViewController.swift
+++ Shipfastr/Views/MapView/MapViewController.swift
@@ -15,17 +15,26 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       GMSServices.provideAPIKey("AIzaSyAljGQx6PV8wK63qHQXjl5FJ3UZDeXta2Y")
-       let camera = GMSCameraPosition(latitude: 49.28273, longitude: -123.120735, zoom: 12)
-       mapView = GMSMapView(frame: .zero, camera: camera)
-       self.view = mapView
-       let marker = GMSMarker()
-       marker.position = CLLocationCoordinate2D(latitude: 49.28273, longitude: -123.120735)
-       marker.title = "Vancouver"
-       marker.snippet = "Canada"
-       marker.map = mapView
-
-       print(GMSServices.openSourceLicenseInfo())    }
+        GMSServices.provideAPIKey("AIzaSyAljGQx6PV8wK63qHQXjl5FJ3UZDeXta2Y")
+        let camera = GMSCameraPosition(latitude: 49.28273, longitude: -123.120735, zoom: 12)
+        mapView = GMSMapView(frame: .zero, camera: camera)
+        
+        do {
+          // Set the map style by passing the URL of the local file.
+          if let styleURL = Bundle.main.url(forResource: "dark-theme", withExtension: "json") {
+            mapView.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
+          } else {
+            NSLog("Unable to find style.json")
+          }
+        } catch {
+          NSLog("One or more of the map styles failed to load. \(error)")
+        }
+        
+        self.view = mapView
+        
+    }
+    
+    
     
 
 }
